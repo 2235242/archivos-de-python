e7ed2fd01b704066eae21e26a85963ed2db9d3d3
2235242 <lopezyulieth745@gmail.com>
2025-03-13 10:19:03 -0500

    Add files via upload

---
+++ ejer_basico_0.3.py
@@ -0,0 +1,10 @@
+##calculo de la media aritmetica
+print("bienvenido")
+print("ingrese el valor del primer numero")
+nume_1= float (input())
+print("ingrese el valor del segundo numero")
+nume_2= float (input())
+print("ingrese el valor del tercer numero")
+nume_3= float (input())
+media= nume_1 + nume_2 + nume_3 / 3
+print("el valor de la media aritmetica es:", media)
---
+++ ejer_basico_0.4.py
@@ -0,0 +1,7 @@
+print("bienvenido al programa")
+print("por favor ingrese su peso en kilogramos")
+peso= float(input())
+print("por favor ingrese su altura en metros")
+altura= float(input())
+imc= peso / altura**2
+print("su indice de masa corporal es:", imc)
