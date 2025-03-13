9f205ac17d690a33b1cf46f2b2cd55242d345877
2235242 <lopezyulieth745@gmail.com>
2025-03-13 12:43:01 -0500

    Add files via upload

---
+++ ejer_basico_10.py
@@ -0,0 +1,7 @@
+##programa para poder hallar el perimetro ya area de un circulo
+diametro= int(input("por favor digite el diametro del circulo: " ))
+radio= (diametro/2)
+perimetro= (2 * 3.14 * radio)
+area= (3.14 * radio**2)
+print("el resultado del perimetro del circulo es:", perimetro)
+print("el resultado del calculo del area del circulo es:", area)
