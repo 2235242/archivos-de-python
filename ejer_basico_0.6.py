53e84a250b2c5e1c5191b28a78bfacb955e9019c
2235242 <lopezyulieth745@gmail.com>
2025-03-13 10:45:45 -0500

    Add files via upload

---
+++ ejer_basico_0.6.py
@@ -0,0 +1,6 @@
+##convercion de faherenheit a grados kelvin
+print("bienvenido al programa")
+print("ingrese el numero de la temperatura en grados Fahrenheit que quiera  convertir a grados kelvin")
+Fahrenheit= float(input())
+kelvin= (Fahrenheit - 32 / 1.8 + 273.15)
+print("la conversion de la temperatura de fahrenheit a celsius es:", kelvin)
