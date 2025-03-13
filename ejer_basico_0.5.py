0da24e4e8038cf85f98254ba3c5340ed619589b4
2235242 <lopezyulieth745@gmail.com>
2025-03-13 10:21:29 -0500

    Add files via upload

---
+++ ejer_basico_0.5.py
@@ -0,0 +1,6 @@
+##convercion de faherenheit a grados celsius
+print("bienvenido al programa")
+print("ingrese el numero de la temperatura en grados Fahrenheit que quiera  convertir")
+Fahrenheit= float(input())
+Celsius= (Fahrenheit - 32 / 1.8)
+print("la conversion de la temperatura de fahrenheit a celsius es:", Celsius)
